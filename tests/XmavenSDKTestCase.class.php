fa4ed347e1b2d4516e78588e04045378afd6be7c
michaelvenatus <michael.kowalczuk@venatusmedia.com>
2015-05-12 14:34:51 +0000

    Remove CleanUp method leftovers

--- tests/XmavenSDKTestCase.class.php
+++ tests/XmavenSDKTestCase.class.php
@@ -19,32 +19,7 @@ class XmavenSDKTestCase extends \PHPUnit_Framework_TestCase
   }
   
   public static function cleanUp() {
-//    $testName = \nano\core\config\Config::get('phpunit_test_name');
-//    
-//    $accounts = \project\db\om\main\AccountTable::retrieveByQuerySortAndLimit(array(
-//      'name' => new \MongoRegex('/^'.$testName.'/'),
-//    ), array());
-//    
-//    foreach ($accounts as $account) {
-//      $account->deletePermanently();
-//    }
-//    
-//    $users = \project\db\om\main\UserTable::retrieveByQueryAndSort(array(
-//      'first_name' => new \MongoRegex('/^'.$testName.'/'),
-//    ), array());
-//    
-//    foreach ($users as $user) {
-//      $user->delete();
-//    }
-  }
-  
-  public function inArrayMatching($needlePattern, $haystack) {
-    foreach ($haystack as $value) {
-      if (preg_match($needlePattern, $value) === 1) {
-        return true;
-      }
-    }
-    return false;
+    
   }
   
   public function assertDateFormat($str) {
